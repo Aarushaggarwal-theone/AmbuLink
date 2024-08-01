a12f190668475ad866c6592096197eef37a904a4
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2024-08-01 16:01:14 +0530

    user + signup code

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, render_template, request, jsonify, flash, redirect, url_for
+from flask import Flask, render_template, request, jsonify, flash, redirect, url_for, make_response
 from utils.db import session, User
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
-
+from utils.db import session, create_user, login_user
 load_dotenv()
 UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'}
@@ -11,19 +11,26 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app.config['SECRET_KEY'] = "rvgkt3delm2bty4io3flejn2k;3kmoij[40t3rfl2jn4ek1;]"
 
-@app.route('/hi', methods=['GET'])
-def hi():
-    return 'Hello, World!'
+
+
+@app.route('/app', methods=['GET'])
+def appfunc():
+    if request.cookies.get('session'):
+        return 'Hello, World!'
+    return redirect(url_for('login'))
 
 @app.route('/home', methods=['GET'])
-def get_users():
-    return render_template('index.html')
+def home():
+   return render_template('index.html')
+    
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -34,7 +41,17 @@ def login_post():
     if request.method=='POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        return f"Email: {email}, Password: {password}"
+        res = login_user(session, email, password)
+        print(res)
+        if res['message']['status']==200:
+            resp = make_response("setting session cookie")
+            resp.set_cookie('session', res['session']["user"])
+            return redirect(url_for('appfunc'))
+        else:
+            return redirect(url_for('login'))
+
+        
+    
 
 @app.route('/signup', methods=['GET'])
 def signup():
@@ -43,10 +60,31 @@ def signup():
 @app.route('/signup', methods=['POST'])
 def signup_post():
     if request.method=='POST':
+        name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
-        return f"Email: {email}, Password: {password}"
+        res = create_user(session, email, password)
+        print(res)
+        if res['message']['status']==200:
+            resp = make_response("setting session cookie")
+            resp.set_cookie('session', res['session']["user"])
+            return redirect(url_for('appfunc'))
+
+        else:
+            return redirect(url_for('signup'))
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.cookies.get('session'):
+        return redirect(url_for('appfunc'))
+    else:
+        return redirect(url_for('home'))
     
+@app.route('/logout', methods=['GET'])
+def logout():
+    resp = make_response("deleting session cookie")
+    resp.delete_cookie('session')
+    return redirect(url_for('login'))
 
 @app.route('/fileupload', methods=['GET'])
 def files():
@@ -67,7 +105,7 @@ def fileupload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('hi'))
+            return redirect(url_for('appfunc'))
         
         if file and not allowed_file(file.filename):
             flash('Invalid file type')
--- utils/db.py
+++ utils/db.py
@@ -1,16 +1,15 @@
 from sqlalchemy import create_engine,  ForeignKey, Column, Integer, String, Text, DateTime, Boolean, Float, CHAR
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 from sqlalchemy.orm import relationship, backref, sessionmaker
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     email = Column(String)
-    user_name = Column(String)
     password = Column(String)
-    
     def __init__(self, id, email, password):
         self.id = id
         self.email = email
@@ -18,8 +17,57 @@ class User(Base):
     
     def __repr__(self):
         return "<User(email='%s', password='%s')>" % (
-            self.email, self.username, self.password)
-        
+            self.email, self.password)
+
+
+class Patient(Base):
+    __tablename__ = 'patients'
+    id = Column(String, primary_key=True)
+    full_name = Column(String)
+    age = Column(Integer)
+    blood_type = Column(String)
+    allergies = Column(String)
+    weight = Column(Float)
+    height = Column(Float)
+    user_id = Column(String, ForeignKey('users.id'))
+    user = relationship("User", backref=backref('patients', order_by=id))
+
+class Contacts(Base):
+    __tablename__ = 'contacts'
+    id = Column(String, primary_key=True)
+    full_name = Column(String)
+    relation = Column(String)
+    phone_number = Column(String)
+    email = Column(String)
+    user_id = Column(String, ForeignKey('users.id'))
+    user = relationship("User", backref=backref('contacts', order_by=id))
+    
+class Hospital(Base):
+    __tablename__ = 'hospitals'
+    id = Column(String, primary_key=True)
+    location = Column(String)
+    phone_number = Column(String)
+    
+class Ambulance(Base):
+    __tablename__ = 'ambulances'
+    id = Column(String, primary_key=True)
+    location = Column(String)
+    phone_number = Column(String)
+    status = Column(Boolean)
+    hospital_id = Column(String, ForeignKey('hospitals.id'))
+    hospital = relationship("Hospital", backref=backref('ambulances', order_by=id))
+    
+class Emergency(Base):
+    __tablename__ = 'emergencies'
+    id = Column(String, primary_key=True)
+    accepted = Column(Boolean)
+    user_id = Column(String, ForeignKey('users.id'))
+    user = relationship("User", backref=backref('emergencies', order_by=id))
+    ambulance_id = Column(String, ForeignKey('ambulances.id'))
+    ambulance = relationship("Ambulance", backref=backref('emergencies', order_by=id))
+    hospital_id = Column(String, ForeignKey('hospitals.id'))
+    hospital = relationship("Hospital", backref=backref('emergencies', order_by=id))
+
 engine = create_engine('sqlite:///db.db', echo=True)
 
 Base.metadata.create_all(bind=engine)
@@ -28,4 +76,35 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
-session.query(User).all()
+import uuid
+
+def create_user(session, email, password):
+    id = str(uuid.uuid4())
+    if session.query(User).filter(User.email==email).first():
+        message = {"message":"User already exists", "status":400}
+        return {"message":message}
+
+    else:
+        user = User(id=id, email=email, password=password)
+        session.add(user)
+        session.commit()
+        message = {"message":"User created successfully", "status":200}
+        
+        session = {"user":id, "expires": str(datetime.datetime.now() + datetime.timedelta(days=1))}
+    
+        return {"message":message, "session":session}
+
+def login_user(session, email, password):
+    user = session.query(User).filter(User.email==email).first()
+    if user:
+        if user.password == password:
+            message = {"message":"Login successful", "status":200}
+            session = {"user":user.id, "expires": str(datetime.datetime.now() + datetime.timedelta(days=1))}
+            return {"message":message, "session":session}
+        else:
+            message = {"message":"Incorrect password", "status":400}
+            return {"message":message}
+    else:  
+        message = {"message":"This email does not exist", "status":400}
+        return {"message":message}
+        
